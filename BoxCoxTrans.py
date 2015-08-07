433e50dad2b0e8ad8859f1929957412c10f39de8
Cam Bruggeman <cam.bruggeman@gmail.com>
2015-08-07 13:25:26 -0400

    Updated BoxCoxTrans

--- BoxCoxTrans.py
+++ BoxCoxTrans.py
@@ -1,5 +1,14 @@
 from scipy.stats import boxcox
 from scipy.stats import boxcox_normmax
+import numpy as np
+
+def boxcoxArray(X,lmbdaList):
+    cols=X.shape[1]
+    return np.array
+
+def boxcoxArrayArgs(X):
+    return [boxcox_normmax(X[:,c]) for c in range(X.shape[1])]
+
 
 class BoxCoxTrans:
     def __init__(self,lmbda=None):
